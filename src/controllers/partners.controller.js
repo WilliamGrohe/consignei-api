c0d48be2af31a24815793482182be7ed2b862bbf
WilliamGrohe <william.grohe@gmail.com>
2026-01-25 20:31:07 -0300

    feat: add partners repository and controller create function

--- src/controllers/partners.controller.js
+++ src/controllers/partners.controller.js
@@ -9,3 +9,28 @@ export async function getPartners(req, res) {
     res.status(500).json({ error: 'Erro ao carregar parceiros' });
   }
 }
+
+export async function createPartner(req, res) {
+  try {
+    // ⚠️ depois isso virá do auth (JWT)
+    const userId = req.user?.id || '11111111-1111-1111-1111-111111111111'; 
+    const partner = req.body;
+
+    if (!partner.name || !partner.cnpj) {
+      return res.status(400).json({
+        error: 'Nome e CNPJ são obrigatórios',
+      });
+    }
+    
+    const newPartner = await PartnersRepo.createPartner({
+      userId,
+      ...partner
+    })
+    return res.status(201).json(newPartner);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      error: 'Erro ao cadastrar parceiro',
+    });
+  }
+}
--- src/repositories/partners.repository.js
+++ src/repositories/partners.repository.js
@@ -7,3 +7,27 @@ export async function getPartners() {
   const { rows } = await pool.query(sql);
   return rows;
 }
+
+export async function createPartner(partner) {
+  const sql = `
+    INSERT INTO partners (
+      user_id,
+      name,
+      contact_name,
+      cnpj,
+      notes
+    )
+    VALUES ($1, $2, $3, $4, $5)
+    RETURNING *
+  `;
+
+  const { rows } = await pool.query(sql, [
+    partner.userId,
+    partner.name,
+    partner.contact_name,
+    partner.cnpj,
+    partner.notes
+  ]);
+
+  return rows[0];
+}
